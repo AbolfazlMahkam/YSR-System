4cbcb70fcd8d9c31f22e741487ece20699fa4ed7
a.mahkam.950 <a.mahkam.9500@gmail.com>
2026-06-27 06:39:54 +0330

    preview uploaded files

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -13,5 +13,11 @@ export default defineConfig({
   server: {
     host: "127.0.0.1",
     port: 4000,
+    proxy: {
+      "/uploads": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
   },
 });
